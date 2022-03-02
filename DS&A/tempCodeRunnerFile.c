5b9ecad5b3a1309cc0a8a3c124425c9466864103
Abdul Rafey <abdulrafeyq@gmail.com>
2022-03-02 11:04:44 +0530

    added time and space complexity in insertion sort

---
+++ DS&A/tempCodeRunnerFile.c
@@ -0,0 +1,18 @@
+printf("Linked List before insertion at first node\n");
+    // linkedListTraversal(head);
+    // printf("\nLinked List After insertion at first node\n");
+    // head = insertAtFirst(head, 87);
+    // linkedListTraversal(head);
+    
+    // printf("Linked List before insertion at end\n");
+    // linkedListTraversal(head);
+    // printf("\nLinked List After insertion at end\n");
+    // head = insertAtEnd(head, 100);
+    // linkedListTraversal(head);
+    
+    // printf("Linked List before insertion at index-2\n");
+    // linkedListTraversal(head);
+    // printf("\nLinked List After insertion at index-2\n");
+    // head = insertAtIndex(head, 555, 2);
+    // linkedListTraversal(head);
+    

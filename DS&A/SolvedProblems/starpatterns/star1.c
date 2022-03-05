71e13f388334f39ecc21b7ba5ee3708367d09d7d
Abdul Rafey <abdulrafeyq@gmail.com>
2022-03-05 10:02:12 +0530

    star pattern 1 using for loops

---
+++ DS&A/SolvedProblems/starpatterns/star1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    int i, j;
+
+    for(i=1;i<=5;i++){
+        for(j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+/*
+i   j
+1   1
+2   1,2
+3   1,2,3
+4   1,2,3,4
+5   1,2,3,4,5
+*/

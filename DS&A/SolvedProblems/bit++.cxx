940294018b0eb091b5a0e6925dc142c5befd76df
Abdul Rafey <abdulrafeyq@gmail.com>
2022-03-11 17:59:44 +0530

    defined question

--- DS&A/SolvedProblems/bit++.cxx
+++ DS&A/SolvedProblems/bit++.cxx
@@ -2,6 +2,11 @@
 #include <string>
 using namespace std;
 
+/*
+ Question: user can perform only two operations increment by 1 or
+ * decrement by 1. print the result after n number of operations 
+ */
+
 int main(int argc, char **argv)
 {
 	int n, x=0;
@@ -19,4 +24,13 @@ int main(int argc, char **argv)
 	
 	return 0;
 }
+
+//Editorial solution
+/*
+ #define all(x) x.begin(), x.end();
+ vector<string> v(n);
+ for(int i=0,i<n;i++)
+	cin>>v[i];
+ cout<<count(all(x),"++X")+count(all(x),"X++")-count(all(x),"--X")-count(all(x),"X--")<<endl;
+ */
 

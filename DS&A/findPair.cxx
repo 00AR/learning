dd6d7c2dfaffbc66406b9904a13ec52228638ec9
Abdul Rafey <abdulrafeyq@gmail.com>
2022-03-23 21:58:14 +0530

    find pair in which the input lies

---
+++ DS&A/findPair.cxx
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n=10, x=-1, y=n,d=0;
+	//float num=4.5;
+	int num=70;
+	//cin>>n;
+	int a[n]={10,21,42,63,74,75,86,87,98,99};
+	//for(int i=0; i<n; i++)
+		//cin>>a[i];
+	while(1){
+		if(num>a[n-1]||num<a[0])
+			break;
+		if(a[x+1]<num)
+			++x;
+		else 
+			break;
+		if(a[y-1]>num)
+			--y;
+		else{
+			break;d=1;}
+	
+	}
+	d ? cout<<a[y-1]<<" "<<a[y]:cout<<a[x]<<" "<<a[x+1];
+	cout<<endl;
+	/*int l=0, m, h=n-1;
+	while(abs(h-l)!=1){
+	m=(h-l)/2;
+	if(a[m]<num)
+		l=m;
+	if(a[m]>num)
+		h=m;
+	//cout<<a[l]<<" "<<a[h];
+	}
+	cout<<a[l]<<" "<<a[h];*/
+	return 0;
+}
+/* 
+ * x=0 y=9	
+ * x=1 y=8
+ * 2	7
+ * 3	6
+ * 4	5
+ * 
+ * */

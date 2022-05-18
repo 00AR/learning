58980777fc1167a4a283578cfadd2921c2b85e2b
00AR <abdulrafeyq@gmail.com>
2022-05-18 22:13:27 +0530

    union of two arrays

---
+++ DS&A/SolvedProblems/unionOfArrays.cxx
@@ -0,0 +1,32 @@
+// Given two arrays a[] and b[] of size n and m respectively.
+// The task is to find union between these two arrays. 
+// only one occurrence of an element must be inserted in the new array.
+
+//Constraints:
+//1 ≤ n, m ≤ 10^5
+//0 ≤ a[i], b[i] < 10^5
+// Expected Time Complexity : O((n+m)log(n+m))
+// Expected Auxilliary Space : O(n+m)
+
+// input
+// 6 2 
+// 85 25 1 32 54 6
+// 85 2
+//Output: 7
+//input
+// 5 3
+// 1 2 3 4 5
+// 1 2 3
+// Output: 5
+#include<bits/stdc++.h>
+using namespace std;
+
+int doUnion(int a[], int n, int b[], int m){
+    
+    return 0;
+}
+int main(){
+
+
+    return 0;
+}

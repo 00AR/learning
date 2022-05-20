e360d9e457c1779a6dd5033dc952c088164c0ccf
00AR <abdulrafeyq@gmail.com>
2022-05-20 20:35:45 +0530

    throw and throws diff

--- java/ThrowThrows.java
+++ java/ThrowThrows.java
@@ -11,8 +11,17 @@ public class ThrowThrows {
 			testExceptionDivide(10, 0);
 		}
 		catch(ArithmeticException e){
-			System.out.println("Arithmetic Exception");
+			System.out.println("Arithmetic Exception from main");
 		}
 	}
 }
 
+// the 'throw' keyword is used to manually throw exception to the
+// calling method
+// 'throws' keyword is used in the function definition to inform
+// the calling method that this method throws the exception.
+// Here, testExceptionDivide throws an exception. if the main method
+// is calling it, then it must have handled the exception. Otherwise,
+// the main method can also throw the exception to JVM.
+// And the method testException testExceptionDivide 'throws' the 
+// exception based on the condition.

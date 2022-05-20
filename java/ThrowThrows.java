7a2067f1645d6fd9b7ae4ef9778d0d2fa2a30403
00AR <abdulrafeyq@gmail.com>
2022-05-20 20:29:16 +0530

    difference between throw and throws keyword

---
+++ java/ThrowThrows.java
@@ -0,0 +1,18 @@
+public class ThrowThrows {
+	
+	public static int testExceptionDivide(int a, int b) throws ArithmeticException{
+		if(a == 0 || b == 0)
+			throw new ArithmeticException();
+		return a/b;
+	}
+	
+	public static void main (String[] args) {
+		try{
+			testExceptionDivide(10, 0);
+		}
+		catch(ArithmeticException e){
+			System.out.println("Arithmetic Exception");
+		}
+	}
+}
+
